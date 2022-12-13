abd85bf5a3a7cc5b482e79e04648096cb234c137
Samandar000 <samandarnfh.2020@gmail.com>
2022-12-13 22:56:09 +0500

    get_user_with_month

--- randomuser.py
+++ randomuser.py
@@ -20,10 +20,10 @@ class RandomUser:
                 user = response.json()['results'][0]
                 dt = user['dob']['date']
                 dat = datetime.strptime(dt[:-5], '%Y-%m-%dT%H:%M:%S') #1958-02-09T05:22:53.242Z
-                # print(user)
-                if dat.year == year :
-                    print(user)
-                    break
+                print(dat)
+                # if dat.year == year :
+                #     print(user)
+                #     break
 
     def get_user_with_month(self, month: int) -> dict:
         '''return user with month
@@ -34,7 +34,17 @@ class RandomUser:
         Returns:   
             dict: user
         '''
-        pass
+        while True:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                user = response.json()['results'][0]
+                dt = user['dob']['date']
+                dat = datetime.strptime(dt[:-5], '%Y-%m-%dT%H:%M:%S') #1958-02-09T05:22:53.242Z
+                # print(dat)
+                if dat.month == month:
+                    print(user)
+                    break
+        
 
     def get_user_with_day(self, day: int) -> dict:
         '''return user with day
@@ -60,7 +70,7 @@ class RandomUser:
 
 
 user = RandomUser('https://randomuser.me/api/')
-print(user.get_user_with_year(1980))
-# print(user.get_user_with_month(12))
+# print(user.get_user_with_year(1980))
+print(user.get_user_with_month(12))
 # print(user.get_user_with_day(13))
 # print(user.get_user_with_weekday('Monday'))
