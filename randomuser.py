ef0faf6be19884149fdee0a4dd7bc76dae9ef774
sobirismatov <name@uzcoder96@gmail.com>
2022-12-19 15:31:49 +0500

    randomuser

--- randomuser.py
+++ randomuser.py
@@ -1,6 +1,7 @@
 from csv import Dialect
 import requests
 from datetime import datetime
+import json
 
 class RandomUser:
     def __init__(self, url: str) -> None:
@@ -101,6 +102,7 @@ class RandomUser:
                 user = response.json()["results"][0]
                 if user["gender"] == gender:
                     return user
+                    
 
     def write_users_to_file(self, file_path: str, gender: str, count: int) -> bool:
         '''write the data of count users whose gender is equal to the given gender to file_path
@@ -117,13 +119,20 @@ class RandomUser:
         Returns:   
             bool: True if it is ok otherwise False
         '''
-        pass
-
-
+        users=[]
+        for i in range(count):
+            user=self.get_user_by_gender(gender) 
+            name=user["name"]
+            data = {"full_name": name['first']+" "+name['last'], "gender": user['gender'], "age": user['dob']['age']}
+            users.append(data)
+        user_str=json.dumps(users , indent=4)
+        with open(file_path,"w") as f:
+            f.write(user_str)
+        return True
 user = RandomUser('https://randomuser.me/api/')
-print(user.get_user_with_year(2990))
-print(user.get_user_with_month(12))
-print(user.get_user_with_day(13))
-print(user.get_user_with_weekday('Monday'))
-print(user.get_user_by_gender("male"))
-print(user.write_users_to_file('users.json', 'male', 10))
+# print(user.get_user_with_year(2990))
+# print(user.get_user_with_month(12))
+# print(user.get_user_with_day(13))
+# print(user.get_user_with_weekday('Monday'))
+# print(user.get_user_by_gender("male"))
+# print(user.write_users_to_file('users.json', 'male', 10))
